e762028e237936961c2ff9d7e646be64230bf368
sijan <sijankasalawat3339@gmail.com>
2022-04-19 12:37:41 +0545

    first commit

---
+++ Triangle.java
@@ -0,0 +1,12 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=1/2*a*b;
+        System.out.println("Area of triangle:"+c);
+
+
+
+
+    }
+}
